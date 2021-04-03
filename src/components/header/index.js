2899e91829d5f6a0c5f470561fbb4b41ffa19700
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-04-03 17:43:57 +0300

    installed material core & icons

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-import GDriveLogo from '../../media/Google-Drive-icon.png'
+import GoogleDriveIcon from '../../media/Google-Drive-icon.png'
 
 const index = () => {
     return (
         <div className='header'>
             <div className="header__logo">
-                <img src={GDriveLogo} alt="Google Drive" />
+                <img src={GoogleDriveIcon} alt="Google Drive" />
                 <span>Drive</span>
             </div>
-            <div className="header__searchContainer"></div>
+            <div className="header__searchContainer">
+            <div className="header__searchBar"></div>
+            </div>
             <div className="header__icons"></div>
             
         </div>

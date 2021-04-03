e3586735b4fc286f84b20189fc55d36a1a92ddb0
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-04-03 19:11:57 +0300

    header css done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/Header.css'
 
 import GoogleDriveIcon from '../../media/Google-Drive-icon.png'
 import ExpandIcon from '@material-ui/icons/ExpandMore';
 import SearchIcon from '@material-ui/icons/Search';
 import HelpOutLineIcon from '@material-ui/icons/HelpOutline';
 import SettingsIcon from '@material-ui/icons/Settings';
-import AppsIcon from '@material-ui/icons/Apps'
+import AppsIcon from '@material-ui/icons/Apps';
 
-const index = () => {
+const index = ({userPhoto}) => {
     return (
         <div className='header'>
             <div className="header__logo">
@@ -27,7 +28,7 @@ const index = () => {
                     <SettingsIcon/>
                 </span>
                 <AppsIcon/>
-               <img src="" alt="User Photo"/>
+               <img src={userPhoto} alt="User Photo"/>
             </div>
 
         </div>

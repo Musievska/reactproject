23a807c6aa982ad02bfdb7e7c0eae2cdd58ba810
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-04-03 17:22:40 +0300

    add google drive logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import GDriveLogo from '../../media/Google-Drive-icon.png'
+
 const index = () => {
     return (
-        <div>
+        <div className='header'>
+            <div className="header__logo">
+                <img src={GDriveLogo} alt="Google Drive" />
+                <span>Drive</span>
+            </div>
+            <div className="header__searchContainer"></div>
+            <div className="header__icons"></div>
             
         </div>
     )

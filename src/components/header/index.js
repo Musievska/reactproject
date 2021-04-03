1dcbfcf20b773139e9e221df1cc7a54de8ceef6c
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-04-03 18:33:26 +0300

    added header icons

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import GoogleDriveIcon from '../../media/Google-Drive-icon.png'
+import ExpandIcon from '@material-ui/icons/ExpandMore';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutLineIcon from '@material-ui/icons/HelpOutline';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AppsIcon from '@material-ui/icons/Apps'
 
 const index = () => {
     return (
@@ -10,10 +15,21 @@ const index = () => {
                 <span>Drive</span>
             </div>
             <div className="header__searchContainer">
-            <div className="header__searchBar"></div>
+                <div className="header__searchBar">
+                    <SearchIcon />
+                    <input type='text' placeholder='Search in Drive'/>
+                    <ExpandIcon />
+                </div>
             </div>
-            <div className="header__icons"></div>
-            
+            <div className="header__icons">
+                <span>
+                    <HelpOutLineIcon/>
+                    <SettingsIcon/>
+                </span>
+                <AppsIcon/>
+               <img src="" alt="User Photo"/>
+            </div>
+
         </div>
     )
 }

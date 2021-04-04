e0cf68cc4a84afeae907c67298d7ab2b9988ebc2
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-04-04 13:24:35 +0300

    adding file metrics

--- src/components/FilesView/FileItem.js
+++ src/components/FilesView/FileItem.js
@@ -5,12 +5,23 @@ import IncertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
     'August', 'September', 'October', 'November', 'December'];
 
-export const FileItem = ({ id, caption, timestamp, fileUrl, size }) => {
+const FileItem = ({ id, caption, timestamp, fileUrl, size }) => {
     const fileDate = `${timestamp?.toDate().getDate()} ${monthNames[timestamp?.toDate().getMonth() + 1]} ${timestamp?.toDate().getFullYear()}`;
-    
+
+    const getReadableFileSize = (fileSizeInBytes) => {
+        let i = -1;
+        const byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+        do {
+            fileSizeInBytes = fileSizeInBytes / 1024;
+            i++;
+        } while (fileSizeInBytes > 1024);
+
+        return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+    };
+
     return (
-        <div>
-            
+        <div className="">
+
         </div>
     )
 }

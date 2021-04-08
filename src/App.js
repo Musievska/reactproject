05828ecf2f85f5878a50e3dd36acc9374d2fbffb
Dilyana Musievska <dilyana.musievska@gmail.com>
2021-04-08 13:10:50 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -31,22 +31,22 @@ function App() {
     <div className="app">
       {
         user ? (
-      <>
-      <Header userPhoto={user.photoURL} />
-      <div className="app__main">
-        <Sidebar />
-        <FilesView />
-        <SideIcons />
+          <>
+            <Header userPhoto={user.photoURL} />
+            <div className="app__main">
+              <Sidebar />
+              <FilesView />
+              <SideIcons />
 
-      </div>
-      </>
-  ) : (
-    <div className="app__login">
-      <img src={GoogleDriveIcon} alt="Google Drive" />
-      <button onClick={handleLogin}>Log in to Google Drive</button>
-    </div>
-  )
-}
+            </div>
+          </>
+        ) : (
+          <div className="app__login">
+            <img src={GoogleDriveIcon} alt="Google Drive" />
+            <button onClick={handleLogin}>Log in to Google Drive</button>
+          </div>
+        )
+      }
     </div>
   );
 }
